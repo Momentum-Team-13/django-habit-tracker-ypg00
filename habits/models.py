6b1908d984471515a55286a277359219a467d3dc
Yuriy Golota <ypgolota@pm.me>
2022-07-11 19:41:08 -0400

    added attributes to Record class

--- habits/models.py
+++ habits/models.py
@@ -16,9 +16,11 @@ class User(BaseModel):
 
 class Habit(BaseModel):
     name = models.CharField(max_length=255)
-    goal = models.IntegerField(max_length=9)
-    metric = models.CharField(max_length=255)
+    goal = models.IntegerField(max_length=9, null=True, blank=True)
+    metric = models.CharField(max_length=255, null=True, blank=True)
     user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
 
 class Record(BaseModel):
-    pass
+    quantity = models.IntegerField(max_length=9, null=True, blank=True)
+    date = models.DateField(max_length=255, null=True, blank=True)
+    habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, null=True, blank=True)
