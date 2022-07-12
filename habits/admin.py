967891b4d5510349de7e76b1c96c74fed45c0a4f
Yuriy Golota <ypgolota@pm.me>
2022-07-12 16:25:56 -0400

    useradmin

--- habits/admin.py
+++ habits/admin.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import User, Habit, Record
 
+admin.site.register(UserAdmin)
 admin.site.register(User)
 admin.site.register(Habit)
 admin.site.register(Record)
