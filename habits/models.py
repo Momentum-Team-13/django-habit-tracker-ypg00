bf92db8e4e7ec3999f9f1541ce84117782e1f5bf
Yuriy Golota <ypgolota@pm.me>
2022-07-12 15:09:16 -0400

    wip

--- habits/models.py
+++ habits/models.py
@@ -1,5 +1,6 @@
 from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
@@ -8,7 +9,7 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-class User(BaseModel):
+class User(AbstractUser):
     pass
 
     def __str__(self):
@@ -16,11 +17,11 @@ class User(BaseModel):
 
 class Habit(BaseModel):
     name = models.CharField(max_length=255)
-    goal = models.IntegerField(max_length=9, null=True, blank=True)
+    goal = models.IntegerField(null=True, blank=True)
     unit = models.CharField(max_length=255, null=True, blank=True)
     creator = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
 
 class Record(BaseModel):
-    quantity = models.IntegerField(max_length=9, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
     date = models.DateField(max_length=255, null=True, blank=True)
     habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, null=True, blank=True)
