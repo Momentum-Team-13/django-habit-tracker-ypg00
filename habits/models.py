3fce4fa17ecabeb8d33e003b80b4c33137f600e2
Yuriy Golota <ypgolota@pm.me>
2022-07-14 15:22:50 -0400

    added default value to Record.date

--- habits/models.py
+++ habits/models.py
@@ -21,7 +21,7 @@ class Habit(BaseModel):
     creator = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
 
 class Record(BaseModel):
-    date = models.DateField()
+    date = models.DateField(default='YYYY-MM-DD')
     quantity = models.IntegerField(default=0, null=True, blank=True)
     habit = models.ForeignKey('Habit', on_delete=models.SET_NULL, null=True, blank=True)
 
