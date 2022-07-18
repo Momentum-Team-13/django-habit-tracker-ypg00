3e1e61fb8fc7427939d5de711f1dfbb8f06c1949
Yuriy Golota <ypgolota@pm.me>
2022-07-17 21:24:57 -0400

    wip: stable version, about to modify models.py

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -8,6 +8,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('auth/', include('registration.backends.simple.urls')),
+
+    # Habits
     path('habits/', habits_views.list_habits, name='list_habits'),
-    path('add_habit/', habits_views.add_habit, name='add_habit'),
+    # path('habits/<int:pk>/', habits_views.habit_detail, name='habit_detail'),
+    path('habits/add/', habits_views.add_habit, name='add_habit'),
+    # path('habits/delete/', habits_views.delete_habit, name='delete_habit'),
+
+    # Records
+    # path('records/', habits_views.list_records, name='list_records'),
+    # path('records/<int:pk>/', habits_views.record_detail, name='record_detail'),
+    # path('records/add/', habits_views.record_detail, name='record_detail'),
 ]
